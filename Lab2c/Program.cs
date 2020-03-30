284ac4f2326bfe6e5b5b353128cdca2fe92b325d
RobertBerg <54758314+RobertBerg@users.noreply.github.com>
2020-03-30 14:29:38 -0400

    Add project files.

---
+++ Lab2c/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab2c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\t From Rob");
+        }
+    }
+}
